e5a8da8a8896dd9843780db4d32b1f4ee139c7ff
MeryAmun <chebesiawah@gmail.com>
2021-01-27 23:21:06 +0100

    complete

--- api/blog/controllers/blog.js
+++ api/blog/controllers/blog.js
@@ -1,8 +1,15 @@
-'use strict';
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
-
-module.exports = {};
+module.exports = {
+  async comment(ctx) {
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.comment.create(data, { files });
+    } else {
+        ctx.request.body.blog = ctx.params.id;
+      entity = await strapi.services.comment.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.comment });
+  },
+};
